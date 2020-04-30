f058919235f48a5ad86dca52f3ffcf6ce3f6f411
yukihiro-kawabata <yukihirochasuke0630@gmail.com>
2020-04-30 11:54:35 +0900

    本ツールに関係のない家計簿を排除

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -11,11 +11,10 @@
         <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('kabigon_manew_xo.ico') }}">
 
         <?php // bootstrap ?>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-        <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-        
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
         <?php // select2 ?>
         <link href="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css" rel="stylesheet" />
         <script src="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.min.js"></script>
--- routes/web.php
+++ routes/web.php
@@ -27,18 +27,3 @@ Route::get('/doc', 'apiController@docAction');
 Route::get('/api', 'apiController@apiAction');
 
 
-// regist of cash
-Route::get('/cash/index', 'cashController@indexAction');
-// regist execute
-Route::get('/cash/indexexecute', 'cashController@indexexecute');
-Route::post('/cash/indexexecute', 'cashController@indexexecute');
-// cash list
-Route::get('/cash/list', 'cashController@listAction');
-// delete of cash list
-Route::get('/cash/deleteexecute', 'cashController@deleteexecute');
-// fetch detail by id
-Route::get('/cash/fetch/detail', 'cashController@fetch_detail_by_id');
-// constant regist cash of list
-Route::get('/cash/constant/list', 'cashController@constantListAction');
-// delete of constant cash data
-Route::get('/cash/constant/deleteexecute', 'cashController@constantDeleteexecute');
